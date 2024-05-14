b78d69373bc4127389df9308c4e79ed1dadb3e4c
fatimaatharkhan <fatimaatharkhan@gmail.com>
2024-05-15 01:27:33 +0500

    Create Product and Review

--- server/models.js
+++ server/models.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
-
-// Counter Schema
 const counterSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  sequence_value: { type: Number, default: 0 },
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
+  sequenceValue: { type: Number, default: 0 },
 });
 
 const Counter = mongoose.model("Counter", counterSchema);
 
-// USER SCHEMA
 const userSchema = new mongoose.Schema({
-  _id: { type: Number },
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
@@ -21,72 +17,38 @@ const userSchema = new mongoose.Schema({
   aboutBusiness: { type: String },
 });
 
-userSchema.pre("save", async function (next) {
-  const doc = this;
-  if (!doc._id) {
-    try {
-      const counter = await Counter.findByIdAndUpdate(
-        { _id: "userId" },
-        { $inc: { sequence_value: 1 } },
-        { new: true, upsert: true }
-      );
-      doc._id = counter.sequence_value;
-      next();
-    } catch (error) {
-      return next(error);
-    }
-  } else {
-    return next();
-  }
-});
-
 const User = mongoose.model("User", userSchema);
 
-// PRODUCT SCHEMA
 const productSchema = new mongoose.Schema({
-  id: {type: Number},
-  userId: { type: Number, ref: "User", required: true }, // Add userId field
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   name: { type: String, required: true },
   category: { type: String },
   description: { type: String },
   price: { type: Number },
   imageUrl: { type: String },
   createdAt: { type: Date, default: Date.now },
-  rating: { type: Number, default: 0 }, // Set default value of rating to 0
+  rating: { type: Number, default: 0 },
 });
 
 const Product = mongoose.model("Product", productSchema);
 
-// REVIEW SCHEMA
 const reviewSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  productId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Product",
-    required: true,
-  },
+  productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
   rating: { type: Number, required: true },
   reviewText: { type: String },
-  imageUrl: { type: String },
   createdAt: { type: Date, default: Date.now },
-  //   updatedAt: { type: Date, default: Date.now }
 });
 
 const Review = mongoose.model("Review", reviewSchema);
 
-// COMMENT SCHEMA
 const commentSchema = new mongoose.Schema({
-  reviewId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Review",
-    required: true,
-  },
+  reviewId: { type: mongoose.Schema.Types.ObjectId, ref: "Review", required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   commentText: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
-  //   updatedAt: { type: Date, default: Date.now }
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
 
-module.exports = { User, Product, Review, Comment };
+module.exports = { User, Product, Review, Comment, Counter };
--- server/routes.js
+++ server/routes.js
@@ -1,38 +1,49 @@
 const express = require("express");
 const router = express.Router();
-const { User, Product } = require("./models");
-const bodyParser = require('body-parser');
+const { User, Product, Review } = require("./models");
+const bodyParser = require("body-parser");
 
 // Use body-parser middleware to parse JSON bodies
 router.use(bodyParser.json());
 
+
+// Authentication middleware
+const authenticateUser = (req, res, next) => {
+    if (req.session.user) {
+      next(); // User is authenticated
+    } else {
+      res.status(401).json({ message: "User not authenticated" }); // User is not authenticated
+    }
+  };
+
+
 // SIGNUP
 router.post("/users/signup", async (req, res) => {
-  try {
-    const { name, email, password, userType } = req.body;
-
-    // Validate request data
-    if (!name || !email || !password || !userType) {
-      return res.status(400).json({ message: "All fields are required" });
+    try {
+      const { name, email, password, userType } = req.body;
+  
+      // Validate request data
+      if (!name || !email || !password || !userType) {
+        return res.status(400).json({ message: "All fields are required" });
+      }
+  
+      // Check if user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: "Email already exists" });
+      }
+  
+      // Create a new user instance using the Mongoose model
+      const user = new User({ name, email, password, userType });
+      await user.save();
+  
+      // Return a success response
+      res.status(201).json({ message: "User created successfully", user });
+    } catch (error) {
+      console.error("Error creating user:", error);
+      res.status(500).json({ message: "Server Error" });
     }
-
-    // Check if user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
-
-    // Create a new user instance using the Mongoose model
-    const user = new User({ name, email, password, userType });
-    await user.save();
-
-    // Return a success response
-    res.status(201).json({ message: "User created successfully", user });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ message: "Server Error" });
-  }
-});
+  });
 
 // LOGIN
 router.post("/users/login", async (req, res) => {
@@ -41,7 +52,9 @@ router.post("/users/login", async (req, res) => {
 
     // Validate request data
     if (!email || !password) {
-      return res.status(400).json({ message: "Email and password are required" });
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     }
 
     // Find the user by email in the database
@@ -54,6 +67,7 @@ router.post("/users/login", async (req, res) => {
 
     // Store user information in the session
     req.session.user = user;
+    console.log(user._id);
 
     // Return a success response
     res.status(200).json({ message: "Login successful", user });
@@ -103,48 +117,93 @@ router.put("/users/profile", async (req, res) => {
     await user.save();
 
     // Return a success response
-    res.status(200).json({ message: "User profile updated successfully", user });
+    res
+      .status(200)
+      .json({ message: "User profile updated successfully", user });
   } catch (error) {
     console.error("Error updating user profile:", error);
     res.status(500).json({ message: "Server Error" });
   }
 });
 
+router.get("/products", async(req,res) => {
+
+return res.status(200).json("was good");
+
+})
+
 // CREATE PRODUCT
 router.post("/products", async (req, res) => {
     try {
-      const { userId, name, category, description, price, imageUrl } = req.body;
-    
+      const { name, category, description, price } = req.body;
+      const userId = req.get("userId");
+      console.log(userId);
+      console.log(!name,!category,!description,!price);
       // Validate request data
       if (!userId || !name || !category || !description || !price) {
-        return res
-          .status(400)
-          .json({
-            message:
-              "userId, name, category, description, and price are required fields",
-          });
+        return res.status(400).json({
+          message:
+            "userId, name, category, description, and price are required fields",
+        });
       }
-    
-      // Parse userId as a number
-      const parsedUserId = parseInt(userId);
+  
+    //   // Parse userId as a number
+    //   const parsedUserId = parseInt(userId);
   
       // Create a new product instance using the Mongoose model
       const product = new Product({
-        userId: parsedUserId,
+        userId,
         name,
         category,
         description,
-        price,
-        imageUrl,
+        price
+        
       });
-    
+  
       // Save the product to the database
       await product.save();
-    
+  
       // Return a success response
       res.status(201).json({ message: "Product created successfully", product });
     } catch (error) {
       console.error("Error creating product:", error);
+      res.status(500).json({ message: "Server Error" });
+    }
+  });
+
+// CREATE REVIEW
+router.post("/products/:productId/reviews", async (req, res) => {
+    try {
+      const { rating, reviewText } = req.body;
+      const productId = req.params.productId;
+      console.log(req.session.user);
+      const userId = req.get("userId");
+      //console.log(req.session.user.);
+      // Validate request data
+      if (!userId || !rating) {
+        return res.status(400).json({
+          message: "userId and rating are required fields",
+        });
+      }
+  
+    //   // Parse productId as a number
+    //   const parsedProductId = parseInt(productId);
+  
+      // Create a new review instance using the Mongoose model
+      const review = new Review({
+        userId,
+        productId,
+        rating,
+        reviewText,
+      });
+  
+      // Save the review to the database
+      await review.save();
+  
+      // Return a success response
+      res.status(201).json({ message: "Review created successfully", review });
+    } catch (error) {
+      console.error("Error creating review:", error);
       res.status(500).json({ message: "Server Error" });
     }
   });
